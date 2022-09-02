4f1d375ecc9e5292be6cbcd1e276b68a690c9af8
Nanang Komarudin <45787278+nanangNSL@users.noreply.github.com>
2022-09-02 17:21:25 +0700

    Fix cors september

--- app.js
+++ app.js
@@ -8,8 +8,15 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const PORT = 5000;
 
+
+
+
+app.disable("x-powered-by");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors());
-const allowlist = ["https://motaro-2020d.web.app", "http://localhost:3000", ];
+const allowlist = ["https://motaro-2020d.web.app", "http://localhost:3000", "https://motaro-2020d.firebaseapp.com"];
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
   if (allowlist.indexOf(req.header("Origin")) !== -1) {
@@ -21,11 +28,6 @@ const corsOptionsDelegate = function (req, callback) {
 };
 app.use(cors(corsOptionsDelegate));
 
-
-app.disable("x-powered-by");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.use("/public", express.static("public"));
 glob.sync("./api/routes/Route.js").forEach((file) => {
